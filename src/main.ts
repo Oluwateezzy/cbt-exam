72ef710e58c5f1a4c8fb5d5c7a1358475dc70532
oluwatobiloba <oluwateezzy03@gmail.com>
2023-09-16 23:21:02 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,17 @@
 import { NestFactory } from '@nestjs/core';
+import {SwaggerModule, DocumentBuilder} from '@nestjs/swagger';
 import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  const config = new DocumentBuilder()
+    .setTitle("Computer Based Test")
+    .setDescription("A computer based test api")
+    .setVersion("1.0")
+    .addBearerAuth()
+    .build();
+    const document = SwaggerModule.createDocument(app, config)
+    SwaggerModule.setup("api", app, document)
   await app.listen(3000);
 }
 bootstrap();
