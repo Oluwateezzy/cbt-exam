ed4b80708c36e6a93155d89adb544d3073259d8e
oluwatobiloba <oluwateezzy03@gmail.com>
2023-09-17 00:00:19 +0100

    update

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,10 +1,13 @@
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
 import { AppService } from './app.service';
+import { AuthController } from '../auth/auth.controller';
+import { UsersController } from 'src/users/users.controller';
+import { AuthService } from 'src/auth/auth.service';
 
 @Module({
   imports: [],
-  controllers: [AppController],
-  providers: [AppService],
+  controllers: [AppController, AuthController, UsersController],
+  providers: [AppService, AuthService, UsersController],
 })
 export class AppModule {}
