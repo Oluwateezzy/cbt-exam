91a8b19daf4ee8d27c36e66eef599a9ea62f0ce5
oluwatobiloba <oluwateezzy03@gmail.com>
2023-10-12 13:02:31 +0100

    update

--- src/main.ts
+++ src/main.ts
@@ -4,6 +4,7 @@ import { AppModule } from './app/app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+  app.enableCors()
   const config = new DocumentBuilder()
     .setTitle("Computer Based Test")
     .setDescription("A computer based test api")
